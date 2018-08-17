cff0e62af24563e1d530288a5a11a588a35065cc
jcha9928 <cha.jiook@gmail.com>
2018-08-16 20:26:44 -0400

    a

--- data/adni/data_merge.R
+++ data/adni/data_merge.R
@@ -4,5 +4,6 @@ d1=read.csv("combine_MConly_wo_dropouts_1_demo.csv",heade=T, skip=0)
 d2=read.csv("combine_new_biomarker_correct_wo_dropouts.csv",heade=T, skip=0)
 d2_select<-d2[1:6]
 
-data<-Reduce(function(...) merge(..., all = F,by="RID"),
+data<-Reduce(function(...) merge(..., all = F,by="RID",all.x=T),
                   list(d1, d2_select))
+write.csv(data,file="combine_MConly_wo_dropouts_0_biomarker.csv",row.names=F)
